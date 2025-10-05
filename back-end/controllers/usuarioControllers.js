a5d1d674d6d8834b6803ac1197e8c0e7f2aeb73b
xoten herself <ana.schotten@estudante.ifms.edu.br>
2025-10-05 14:33:16 -0300

    05/10 - 14:35

--- back-end/controllers/usuarioControllers.js
+++ back-end/controllers/usuarioControllers.js
@@ -7,8 +7,8 @@ const client = new PrismaClient();
 
 class userController {
 
+    //Cadastrar Usuário
     static async cadastrar(req, res) {
-
         console.log(req.body);
         const { nome, email, senha } = req.body;
 
@@ -26,11 +26,10 @@ class userController {
         res.json({
             userId: user.id,
         });
-
     }
 
+    //Login
     static async login(req, res) {
-
         const { email, senha } = req.body;
 
         const user = await client.user.findUnique({
@@ -53,13 +52,65 @@ class userController {
             });
         }
 
-        const token = jwt.sign({ id: user.id }, process.env.SENHA_SERVIDOR, { expiresIn: "1h" });
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.SENHA_SERVIDOR,
+            { expiresIn: "1h" }
+        );
 
         res.json({
             msg: "Autenticado com sucesso!",
             token: token,
         });
     }
+
+    //Verificar Autenticação
+    static async verificarAuth(req, res, next) {
+        const authHeader = req.headers["authorization"];
+
+        if (authHeader) {
+            const token = authHeader.split(" ")[1];
+
+            jwt.verify(token, process.env.SENHA_SERVIDOR, (err, payload) => {
+                if (err) {
+                    return res.json({
+                        msg: "Token inválido.",
+                    });
+                }
+
+                req.userId = payload.id;
+                next();
+            });
+
+        } else {
+            return res.json({
+                msg: "Token não encontrado.",
+            });
+        }
+    }
+
+    //Verificar Status de Administrador
+    static async verificarAdminStatus(req, res, next) {
+        if (!req.userId) {
+            return res.json({
+                msg: "Você não está autenticado.",
+            });
+        }
+
+        const user = await client.user.findUnique({
+            where: {
+                id: req.userId,
+            },
+        });
+
+        if (!user.adminStatus) {
+            return res.json({
+                msg: "Acesso negado. Usuário não possui privilégios de administrador.",
+            });
+        }
+
+        next();
+    }
 }
 
-module.exports = userController
+module.exports = userController;
