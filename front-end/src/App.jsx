556b2adfacb87545838bace288e8c15f8195e977
xoten herself <ana.schotten@estudante.ifms.edu.br>
2025-10-16 20:11:18 -0400

    16/10 - 20:10

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
