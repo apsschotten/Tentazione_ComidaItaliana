5ce311715f8a570a2cf51e278a476d54e05845ec
Ana Schotten <ana.schotten@estudante.ifms.edu.br>
2025-10-30 16:38:05 -0400

    30/10 - 16:40

--- front-end/src/Pages/Login/index.jsx
+++ front-end/src/Pages/Login/index.jsx
@@ -1,8 +1,46 @@
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
 import styles from './Login.module.css';
+import { useState } from 'react';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('/user/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, senha }),
+        });
+
+        const data = await response.json();
+        console.log(data);
+    };
+
     return (
-        <div className={styles.login}>
+        <div>
+            <Header />
+            <div className={styles.login}>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='email'
+                        placeholder='E-mail'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type='senha'
+                        placeholder='Senha'
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <button type="submit">Fazer Login</button>
+                </form>
+            </div>
+            <Footer />
         </div>
     )
 }
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 
 import App from './App';
+import Login from './Pages/Login/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} path='/' />
+        <Route element={<Login />} path='/login' />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
