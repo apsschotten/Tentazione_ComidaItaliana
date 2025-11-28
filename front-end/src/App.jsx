f1fafb48608a3397be40069243ca5f3938279a8c
xoten herself <ana.schotten@estudante.ifms.edu.br>
2025-11-27 22:00:26 -0400

    27/11 - 22:00

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './Components/Footer';
 
 export default function App() {
   return (
-    <div>
+    <div className='container'>
       <Header />
 
       <main>
--- front-end/src/Pages/Login/index.jsx
+++ front-end/src/Pages/Login/index.jsx
@@ -4,6 +4,13 @@ export default function Login() {
     return (
         <div>
             <h1>Login</h1>
+            <label>
+                E-mail: <input name="email" placeholder='E-mail' />
+            </label>
+            <label>
+                Senha: <input name="senha" placeholder='Senha'/>
+            </label>
+            
         </div>
     );
 }
